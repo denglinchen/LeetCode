931cae7c2e7a0e7d4c944f7ae28b92438195d5b3
denglinchen <515143675@qq.com>
2020-11-20 09:51:52 +0800

    添加653（还未完成）

---
+++ LeetCode/653_两数字之和IV-输入BST.h
@@ -0,0 +1,14 @@
+#pragma once
+/*
+完成时间2020年11月20日
+题目地址：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+*/
+#include"LCInclude.h"
+using namespace std;
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+
+    }
+};
--- LeetCode/LCInclude.h
+++ LeetCode/LCInclude.h
@@ -2,12 +2,26 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<map>
+#include<set>
+#include<unordered_set>
 using namespace std;
 
-//Definition for singly - linked list.
+// Definition for singly - linked list.
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
  };
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
  
